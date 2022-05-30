966b12131d471675f19f3445b1a78c7dac15cb58
Joshua-Troy Meneses <jtmenese@gmail.com>
2022-05-30 15:55:58 -0700

    build: added more sound effects

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -26,18 +26,37 @@ public class PlayerController : MonoBehaviour
   [SerializeField]
   private bool jumped = false;
 
+  [SerializeField]
+  private float attackDelay = 1f;
+
+  [SerializeField]
+  private float timeSinceLastAttack = 1f;
+
+  [SerializeField]
+  private BoxCollider hurtBox;
+
 
   private void Start()
   {
     controller = gameObject.GetComponent<CharacterController>();
-    var t = GetComponent<PlayerInput>().playerIndex;
-    Debug.Log("NEW INDEX" + t);
+    // var t = GetComponent<PlayerInput>().playerIndex;
+    // Debug.Log("NEW INDEX" + t);
   }
 
   public void OnMove(InputAction.CallbackContext ctx)
   {
     movementInput = ctx.ReadValue<Vector2>();
-    Debug.Log(movementInput);
+    if (movementInput.x > 0)
+    {
+      transform.rotation = new Quaternion(0, 0, 0, 1);
+    }
+    else if (movementInput.x < 0)
+    {
+      transform.rotation = new Quaternion(0, 180, 0, 1);
+    }
+
+
+    // Debug.Log(movementInput);
   }
 
   public void OnJump(InputAction.CallbackContext ctx)
@@ -47,8 +66,29 @@ public class PlayerController : MonoBehaviour
     Debug.Log("JUMPED!");
   }
 
+  public void OnAttack(InputAction.CallbackContext ctx)
+  {
+    if (timeSinceLastAttack >= attackDelay)
+    {
+      timeSinceLastAttack = 0f;
+      // hurtBox.gameObject.SetActive(true);
+      StartCoroutine(SetHurtBoxActive());
+      // hurtBox.gameObject.SetActive(false);
+    }
+  }
+
+  IEnumerator SetHurtBoxActive()
+  {
+    Debug.Log("Starting hurtbox coroutine");
+    hurtBox.gameObject.SetActive(true);
+    yield return new WaitForSecondsRealtime(0.2f);
+    Debug.Log("Ending hurtbox coroutine");
+    hurtBox.gameObject.SetActive(false);
+  }
+
   void Update()
   {
+    timeSinceLastAttack += Time.deltaTime;
     groundedPlayer = controller.isGrounded;
     if (groundedPlayer && playerVelocity.y < 0)
     {
@@ -56,7 +96,7 @@ public class PlayerController : MonoBehaviour
     }
 
     // Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-    Vector3 move = new Vector3(movementInput.x, movementInput.y);
+    Vector3 move = new Vector3(movementInput.x, 0);
     controller.Move(move * Time.deltaTime * playerSpeed);
 
     // if (move != Vector3.zero)
--- Assets/Scripts/PlayerSpawner.cs
+++ Assets/Scripts/PlayerSpawner.cs
@@ -13,14 +13,8 @@ public class PlayerSpawner : MonoBehaviour
     var t = PlayerInput.Instantiate(_PlayerObject, -1, "Keyboard 1");
     t.gameObject.transform.position = new Vector3(-17f, -7f, 0f);
     var x = PlayerInput.Instantiate(_PlayerObject, -1, "Controller");
-    x.gameObject.transform.position = new Vector3(-2f, -7f, 0f);
-    // PlayerInput.Instantiate(_PlayerObject);
+    x.gameObject.transform.position = new Vector3(17f, -7f, 0f);
 
-    // _InputManager.playerPrefab = _PlayerObject;
-    // Instantiate(_PlayerObject, new Vector3(-17.3700008f, -7f, 0f), Quaternion.identity);
-    // _InputManager.JoinPlayer();
-    // Instantiate(_PlayerObject, new Vector3(15.5100002f, -7f, 0f), Quaternion.identity);
-    // _InputManager.JoinPlayer();
     Debug.Log("SPAWNED PLAYERS!");
   }
 
