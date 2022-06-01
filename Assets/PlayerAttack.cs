1170d05ff172ba1b65eaec81f2e751e81127ee75
Joshua-Troy Meneses <jtmenese@gmail.com>
2022-05-31 20:37:13 -0700

    feat: player attack correctly reads collision for OnTriggerEnter2D

---
+++ Assets/PlayerAttack.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+  private void OnTriggerEnter2D(Collider2D other)
+  {
+    Debug.Log("HIT");
+    // if (other.CompareTag("Player"))
+    // {
+    //   Debug.Log("HIT PLAYER");
+    // }
+  }
+
+}
